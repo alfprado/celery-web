6832e665ce6817b6fa1e82c7fa369a588dad20b2
André Luiz Fernandes do Prado <prado_fernandes@yahoo.com.br>
2021-06-16 21:47:19 -0300

    renomeia pasta projeto

--- celery-web/util/util.py
+++ celery-web/util/util.py
@@ -1,15 +1,15 @@
 import time
 import pandas as pd
-
 import os
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 DB_FILE = os.path.join(BASE_DIR, 'database.csv')
+OUTPUT = os.path.join(BASE_DIR, 'output')
 
 def create_file(id_task, content):
     timestr = time.strftime("%Y%m%d")
     df = pd.DataFrame(content)
-    df.to_csv(f'output/{id_task}-{timestr}.csv',index=False)
+    df.to_csv(f'{OUTPUT}/{id_task}-{timestr}.csv',index=False)
 
 def read_db():
     data = []
